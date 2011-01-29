a26f35360caf3c295b09f7172d8eacc302c38e8b
Barry Frost <barry@Indigo.local>
2011-01-29 19:35:53 +0000

    Group by date, paging and more.

--- hub.rb
+++ hub.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 require 'active_record'
+require 'parsedate'
   
 configure do
   dbconfig = YAML.load(File.read('config/database.yml'))
@@ -18,23 +19,61 @@ def get_tweets(screen_name='barryf',count=10)
   JSON.parse(resp.body)
 end
 
+def get_links(user='barryf',count=10)
+  url = "http://feeds.delicious.com/v2/json/#{user}"
+  resp = Net::HTTP.get_response(URI.parse(url))
+  JSON.parse(resp.body)
+end
+
+def parse_tweet(tweet)
+  # urls
+  re = Regexp.new('(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)')
+  tweet.gsub!(re, '\1<a href="\2">\2</a>')
+  # usernames
+  re = Regexp.new('(\@)([\w]+)')
+  tweet.gsub!(re, '<a href="http://twitter.com/\2">@\2</a>')
+  # hashtags
+  re = Regexp.new('(\#)([\w]+)')
+  tweet.gsub!(re, '<a href="http://twitter.com/search/\2">@\2</a>')
+  tweet
+end
+
 get '/build/?' do
-  ['barryf','globaldev'].each do |account|
-    tweets = get_tweets(account)
-    tweets.each do |t|
-      if !Item.find_by_uid(t['id'].to_s)
-        Item.create(:uid => t['id'],
-                    :title => t['text'],
-                    :body => t['text'],
-                    :created_at => t['created_at'])
-      end
+  # import tweets
+  tweets = get_tweets
+  tweets.each do |remote|
+    if !Item.find_by_uid(remote['id'].to_s)
+      Item.create(:uid => remote['id'],
+                  :title => remote['text'],
+                  :body => remote['text'],
+                  :source => 'twitter',
+                  :imported_at => Time.now,
+                  :created_at => remote['created_at'])
+    end
+  end
+  # import links
+  links = get_links
+  links.each do |remote|
+    if !Item.find_by_uid(remote['u'].hash.to_s)
+      Item.create(:uid => remote['u'].hash,
+                  :title => remote['d'],
+                  :body => remote['n'],
+                  :url => remote['u'],
+                  :tags => remote['t'].join(' '),
+                  :source => 'delicious',
+                  :imported_at => Time.now,
+                  :created_at => remote['dt'])
     end
   end
   "Built"
 end
 
 get '/' do
-  @items = Item.find(:all, :order => 'created_at DESC')
+  ts = params[:ts] ||= 99999999999
+  @items = Item.find(:all, 
+                     :conditions => ['extract(epoch from created_at) < ?', ts],
+                     :limit => 10,
+                     :order => 'created_at DESC')
   erb :index
 end
 
