856392d7cdae18775dab0835e02be1eb2a405fa7
Barry Frost <public@barryfrost.com>
2011-05-07 12:04:25 +0100

    Checking that imports from external services return a 200 before attempting to parse the JSON. Delicious had started to return 500s.

--- fetchers.rb
+++ fetchers.rb
@@ -82,32 +82,34 @@ end
 # import tweets
 
 def fetch_twitter(count=5, screen_name=ACCOUNTS['twitter']['screen_name'])
+  imported = 0
   url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{screen_name}&include_rts=1&trim_user=1&count=#{count}"
   resp = Net::HTTP.get_response(URI.parse(url))
-  twitter = JSON.parse(resp.body)
-  source = 'twitter'
-  imported = 0
-  twitter.each do |remote|
-    # don't import replies/mentions
-    if Item.where('uid = ? and source = ?', remote['id'].to_s, source).count.zero? && remote['text'][0..0] != '@'
-      # parse tweet and get any oembed data
-      tweet, oembed = parse_tweet(remote['text'])
-      # get co-ordinates and place name if available
-      geo_name = remote['place'] ? remote['place']['full_name'] : ""
-      geo_lat = remote['coordinates'] ? remote['coordinates']['coordinates'][1] : nil
-      geo_lng = remote['coordinates'] ? remote['coordinates']['coordinates'][0] : nil
-      # strip newlines from oembed json
-      oembed = oembed.to_s.gsub(/\n/,'')
-      Item.create(:uid => remote['id'].to_s,
-                  :body => tweet,
-                  :oembed => oembed,
-                  :geo_name => geo_name,
-                  :geo_lat => geo_lat,
-                  :geo_lng => geo_lng,
-                  :source => source,
-                  :imported_at => Time.now,
-                  :created_at => remote['created_at'])
-      imported += 1
+  if resp.code == '200'
+    twitter = JSON.parse(resp.body)
+    source = 'twitter'
+    twitter.each do |remote|
+      # don't import replies/mentions
+      if Item.where('uid = ? and source = ?', remote['id'].to_s, source).count.zero? && remote['text'][0..0] != '@'
+        # parse tweet and get any oembed data
+        tweet, oembed = parse_tweet(remote['text'])
+        # get co-ordinates and place name if available
+        geo_name = remote['place'] ? remote['place']['full_name'] : ""
+        geo_lat = remote['coordinates'] ? remote['coordinates']['coordinates'][1] : nil
+        geo_lng = remote['coordinates'] ? remote['coordinates']['coordinates'][0] : nil
+        # strip newlines from oembed json
+        oembed = oembed.to_s.gsub(/\n/,'')
+        Item.create(:uid => remote['id'].to_s,
+                    :body => tweet,
+                    :oembed => oembed,
+                    :geo_name => geo_name,
+                    :geo_lat => geo_lat,
+                    :geo_lng => geo_lng,
+                    :source => source,
+                    :imported_at => Time.now,
+                    :created_at => remote['created_at'])
+        imported += 1
+      end
     end
   end
   imported
@@ -116,22 +118,24 @@ end
 # import links from delicious
 
 def fetch_delicious(count=5, user=ACCOUNTS['delicious']['user'])
+  imported = 0
   url = "http://feeds.delicious.com/v2/json/#{user}?count=#{count}"
   resp = Net::HTTP.get_response(URI.parse(url))
-  delicious = JSON.parse(resp.body)
-  source = 'delicious'
-  imported = 0
-  delicious.each do |remote|
-    if Item.where('uid = ? and source = ?', remote['u'].hash.to_s, source).count.zero?
-      Item.create(:uid => remote['u'].hash.to_s,
-                  :title => remote['d'],
-                  :body => remote['n'],
-                  :url => remote['u'],
-                  :tags => remote['t'].join(' '),
-                  :source => source,
-                  :imported_at => Time.now,
-                  :created_at => remote['dt'])
-      imported += 1
+  if resp.code == '200'
+    delicious = JSON.parse(resp.body)
+    source = 'delicious'
+    delicious.each do |remote|
+      if Item.where('uid = ? and source = ?', remote['u'].hash.to_s, source).count.zero?
+        Item.create(:uid => remote['u'].hash.to_s,
+                    :title => remote['d'],
+                    :body => remote['n'],
+                    :url => remote['u'],
+                    :tags => remote['t'].join(' '),
+                    :source => source,
+                    :imported_at => Time.now,
+                    :created_at => remote['dt'])
+        imported += 1
+      end
     end
   end
   imported
@@ -140,22 +144,24 @@ end
 # import last.fm loved tracks
 
 def fetch_lastfm(count=5, user=ACCOUNTS['lastfm']['user'], api_key=ACCOUNTS['lastfm']['api_key'])
+  imported = 0
   url = "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&format=json&user=#{user}&api_key=#{api_key}&limit=#{count}"
   resp = Net::HTTP.get_response(URI.parse(url))
-  lastfm = JSON.parse(resp.body)
-  source = 'lastfm'
-  imported = 0
-  lastfm['lovedtracks']['track'].each do |remote|
-    if Item.where('uid = ? and source = ?', remote['url'].hash.to_s, source).count.zero?
-      thumbnail_url = remote.has_key?('image') ? remote['image'][1]['#text'] : ''
-      Item.create(:uid => remote['url'].hash.to_s,
-                  :title => '&lsquo;' + remote['name'] + '&rsquo; by ' + remote['artist']['name'],
-                  :url => remote['url'],
-                  :thumbnail_url => thumbnail_url,
-                  :source => source,
-                  :imported_at => Time.now,
-                  :created_at => Time.at(remote['date']['uts'].to_i))
-      imported += 1
+  if resp.code == '200'
+    lastfm = JSON.parse(resp.body)
+    source = 'lastfm'
+    lastfm['lovedtracks']['track'].each do |remote|
+      if Item.where('uid = ? and source = ?', remote['url'].hash.to_s, source).count.zero?
+        thumbnail_url = remote.has_key?('image') ? remote['image'][1]['#text'] : ''
+        Item.create(:uid => remote['url'].hash.to_s,
+                    :title => '&lsquo;' + remote['name'] + '&rsquo; by ' + remote['artist']['name'],
+                    :url => remote['url'],
+                    :thumbnail_url => thumbnail_url,
+                    :source => source,
+                    :imported_at => Time.now,
+                    :created_at => Time.at(remote['date']['uts'].to_i))
+        imported += 1
+      end
     end
   end
   imported
@@ -164,22 +170,23 @@ end
 # import youtube favorites tracks
 
 def fetch_youtube(count=5, user=ACCOUNTS['youtube']['user'])
+  imported = 0
   url = "http://gdata.youtube.com/feeds/api/users/#{user}/favorites?v=2&alt=json&max-results=#{count}"
   resp = Net::HTTP.get_response(URI.parse(url))
-  youtube = JSON.parse(resp.body)
-  # import to database  
-  source = 'youtube'
-  imported = 0
-  youtube['feed']['entry'].each do |remote|
-    if Item.where('uid = ? and source = ?', remote['id']['$t'].hash.to_s, source).count.zero?
-      Item.create(:uid => remote['id']['$t'].hash.to_s,
-                  :title => "&lsquo;#{remote['title']['$t']}&rsquo;",
-                  :url => remote['link'][0]['href'],
-                  :thumbnail_url => remote['media$group']['media$thumbnail'][1]['url'],
-                  :source => source,
-                  :imported_at => Time.now,
-                  :created_at => remote['published']['$t'])
-      imported += 1
+  if resp.code == '200'
+    youtube = JSON.parse(resp.body)
+    source = 'youtube'
+    youtube['feed']['entry'].each do |remote|
+      if Item.where('uid = ? and source = ?', remote['id']['$t'].hash.to_s, source).count.zero?
+        Item.create(:uid => remote['id']['$t'].hash.to_s,
+                    :title => "&lsquo;#{remote['title']['$t']}&rsquo;",
+                    :url => remote['link'][0]['href'],
+                    :thumbnail_url => remote['media$group']['media$thumbnail'][1]['url'],
+                    :source => source,
+                    :imported_at => Time.now,
+                    :created_at => remote['published']['$t'])
+        imported += 1
+      end
     end
   end
   imported
@@ -188,24 +195,25 @@ end
 # import flickr photos
 
 def fetch_flickr(count=5, user_id=ACCOUNTS['flickr']['user_id'], api_key=ACCOUNTS['flickr']['api_key'])
+  imported = 0
   url = "http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&format=json&api_key=#{api_key}&user_id=#{user_id}&extras=date_taken,geo,url_sq,description&per_page=#{count}"
   resp = Net::HTTP.get_response(URI.parse(url))
-  # remove callback function name and trailing bracket
-  clean = resp.body.sub(/jsonFlickrApi\(/, '').sub(/\)$/,'')
-  flickr = JSON.parse(clean)
-  # import to database
-  source = 'flickr'
-  imported = 0
-  flickr['photos']['photo'].each do |remote|
-    if Item.where('uid = ? and source = ?', remote['id'].to_s, source).count.zero?
-      Item.create(:uid => remote['id'].to_s,
-                  :title => remote['title'],
-                  :url => "http://www.flickr.com/photos/#{ACCOUNTS['flickr']['user']}/#{remote['id']}",
-                  :thumbnail_url => remote['url_sq'],
-                  :source => source,
-                  :imported_at => Time.now,
-                  :created_at => remote['datetaken'])
-      imported += 1
+  if resp.code == '200'
+    # remove callback function name and trailing bracket
+    clean = resp.body.sub(/jsonFlickrApi\(/, '').sub(/\)$/,'')
+    flickr = JSON.parse(clean)
+    source = 'flickr'
+    flickr['photos']['photo'].each do |remote|
+      if Item.where('uid = ? and source = ?', remote['id'].to_s, source).count.zero?
+        Item.create(:uid => remote['id'].to_s,
+                    :title => remote['title'],
+                    :url => "http://www.flickr.com/photos/#{ACCOUNTS['flickr']['user']}/#{remote['id']}",
+                    :thumbnail_url => remote['url_sq'],
+                    :source => source,
+                    :imported_at => Time.now,
+                    :created_at => remote['datetaken'])
+        imported += 1
+      end
     end
   end
   imported
